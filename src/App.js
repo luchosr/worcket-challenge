9f8c7d5f1b6062975962fc43f558f5240964aaef
Luciano <luchosr@gmail.com>
2019-07-11 16:12:40 -0300

    add modal component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Dialog from "./components/Dialog";
 
 function App() {
+  const [isOpen, setisOpen] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setisOpen(true)}>Open Dialog</button>
+      <Dialog isOpen={isOpen} onClose={() => setisOpen(false)}>
+        <h1> Add Item</h1>
+
+        <div>
+          <button>Close</button>
+          <button>Add</button>
+        </div>
+      </Dialog>
     </div>
   );
 }
